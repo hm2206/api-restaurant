93e195e17132a17821292d645c81aa87ee48320b
Hans <twd2206@gmail.com>
2021-08-17 23:39:46 -0500

    add system, clients and methos => migrations

---
+++ database/migrations/1629258399985_systems.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Systems extends BaseSchema {
+  protected tableName = 'systems'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('token').unique();
+      table.string('name').unique();
+      table.text('description');
+      table.string('email').unique();
+      table.string('host').notNullable();
+      table.string('version').notNullable();
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1629259008863_clients.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Clients extends BaseSchema {
+  protected tableName = 'clients'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable().unique();
+      table.text('description');
+      table.string('token').unique();
+      table.enum('device', ['WEB', 'MOBILE', 'DESKTOP', 'CONSOLE']).notNullable()
+      table.string('version').notNullable().defaultTo('0.0.1');
+
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
