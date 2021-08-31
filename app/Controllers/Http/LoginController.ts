2e1d6beaf166e1c4a42a52ec6e0fe437ad239671
Hans <twd2206@gmail.com>
2021-08-30 23:44:03 -0500

    custom login

--- app/Controllers/Http/LoginController.ts
+++ app/Controllers/Http/LoginController.ts
@@ -1,5 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import Hash from '@ioc:Adonis/Core/Hash'
+import User from 'App/Models/User'
+import PaymentRequiredException from '../../Exceptions/PaymentRequiredException';
+import PaymentItem from '../../Helpers/PaymentItem'
 
 export default class LoginController {
 
@@ -12,6 +16,12 @@ export default class LoginController {
         })
 
         const payload = await request.validate({ schema: schemaLogin });
+        
+        const user = await User.findBy('email', payload.email);
+        if (!user) throw new PaymentRequiredException([new PaymentItem("email", "El usuario no existe")])
+
+        const isMatch = await Hash.verify(user.password, payload.password)
+        if (!isMatch) throw new PaymentRequiredException([new PaymentItem("password", "La contraseña es incorrecta")])
 
         const ip_address = request.ip();
         const token = await auth.attempt(payload.email, payload.password, { ip_address });
